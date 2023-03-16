7b5bd6ea8f77cf8b564d557573f6f5b8b66757ef
jameson lin <jameson.lin@student.hu.nl>
2023-03-16 22:35:01 +0100

    Updated eindopdracht.py, gitignore and split

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -8,8 +8,7 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from keras.utils import to_categorical
 from keras.losses import binary_crossentropy
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-# from keras.models import load_model
-
+from keras.models import load_model
 
 def customLoss(y_true, y_pred):
     # Split the true and predicted labels into classification and bounding box coordinates
@@ -56,15 +55,12 @@ def readData(path):
         ymin = annotation['ymin']
         xmax = annotation['xmax']
         ymax = annotation['ymax']
-        
-        unique_classes = set(labels)
-        num_unique_classes = len(unique_classes)
 
         # Preprocess the image
         image = Image.open(path + image_path)
         image = image.resize(target_size)
         image_data = np.array(image, dtype=np.float32)
-        image_data /= 255.0
+        # image_data /= 255.0
 
         # Normalize bounding box coordinates
         height, width, channels = image_data.shape
@@ -80,69 +76,147 @@ def readData(path):
     X_img = np.array(X_img)
     y_labels = np.array(y_labels)
     y_bboxes = np.array(y_bboxes)
-    
-    print(num_unique_classes)
 
     return X_img, y_labels, y_bboxes
 
-
-num_classes = 3 # doesn't see trafficLight-Red and trafficLight-Green, the cause could be the - in between the class name
+num_classes = 5
 num_coords = 4
 
-trainImg, trainLabels, trainbboxes = readData('dataset/train/')
+trainImg, trainLabels, trainbboxes = readData('dataset2/train/')
 trainLabels_cat = np.concatenate([trainLabels[:, np.newaxis], trainbboxes], axis=1)
 trainLabels_cls = to_categorical(trainLabels_cat[:, 0], num_classes)
 
-validImg, validLabels, validbboxes = readData('dataset/valid/')
+validImg, validLabels, validbboxes = readData('dataset2/valid/')
 validLabels_cat = np.concatenate([validLabels[:, np.newaxis], validbboxes], axis=1)
 validLabels_cls = to_categorical(validLabels_cat[:, 0], num_classes)
 
-testImg, testLabels, testbboxes = readData('dataset/test/')
+testImg, testLabels, testbboxes = readData('dataset2/test/')
 testLabels_cat = np.concatenate([testLabels[:, np.newaxis], testbboxes], axis=1)
 testLabels_cls = to_categorical(testLabels_cat[:, 0], num_classes)
 
-# # Add bounding box coordinates to the classification labels
-# trainLabels_cls_reg = np.concatenate([trainLabels_cls, trainbboxes], axis=1)
-# validLabels_cls_reg = np.concatenate([validLabels_cls, validbboxes], axis=1)
-# testLabels_cls_reg = np.concatenate([testLabels_cls, testbboxes], axis=1)
 
-# # Define the model architecture
-# model = Sequential([
-#     Conv2D(32, (3, 3), input_shape=(224, 224, 3)),
-#     MaxPooling2D(pool_size=(2, 2)),
-#     Conv2D(64, (3, 3)),
-#     MaxPooling2D(pool_size=(2, 2)),
-#     Conv2D(128, (3, 3)),
-#     Conv2D(128, (3, 3)),
-#     MaxPooling2D(pool_size=(2, 2)),
-#     Flatten(),
-#     Dense(256, activation='relu'),
-#     Dense(256, activation='relu'),
-#     Dense(num_classes + num_coords, activation='linear')
-# ])
+# Add bounding box coordinates to the classification labels
+trainLabels_cls_reg = np.concatenate([trainLabels_cls, trainbboxes], axis=1)
+validLabels_cls_reg = np.concatenate([validLabels_cls, validbboxes], axis=1)
+testLabels_cls_reg = np.concatenate([testLabels_cls, testbboxes], axis=1)
 
-# # Compile the model with custom loss and Adam optimizer
-# model.compile(loss=customLoss, optimizer='adam', metrics=['accuracy'])
+# Define the model architecture
+model = Sequential([
+    Conv2D(32, (3, 3), input_shape=(224, 224, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(64, (3, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(128, (3, 3)),
+    Conv2D(128, (3, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(256, activation='relu'),
+    Dense(256, activation='relu'),
+    Dense(num_classes + num_coords, activation='linear')
+])
 
-# # Train the model on the training data
-# early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='min', restore_best_weights=True)
-# checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
-# model.fit(trainImg, trainLabels_cls_reg, epochs=50, validation_data=(validImg, validLabels_cls_reg), callbacks=[early_stop, checkpoint])
+# Compile the model with custom loss and Adam optimizer
+model.compile(loss=customLoss, optimizer='adam', metrics=['accuracy'])
 
-# # Evaluate the model on the test data
-# test_loss, test_acc = model.evaluate(testImg, testLabels_cls_reg, verbose=2)
-# print(test_loss)
-# print(test_acc)
+# Train the model on the training data
+early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='min', restore_best_weights=True)
+checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+model.fit(trainImg, trainLabels_cls_reg, epochs=3, validation_data=(validImg, validLabels_cls_reg), callbacks=[early_stop, checkpoint])
 
-# # Save the trained model
-# # model.save('first_test.h5')
+# Evaluate the model on the test data
+test_loss, test_acc = model.evaluate(testImg, testLabels_cls_reg, verbose=2)
+print(test_loss)
+print(test_acc)
 
-# # # Load the best saved model
-# # best_model = load_model('best_model.h5')
+# Predict bounding boxes on the test data
+testPreds = model.predict(testImg)
+
+# Extract predicted bounding box coordinates
+testPreds_cls = testPreds[:, :num_classes]
+testPreds_reg = testPreds[:, num_classes:]
+print('rest')
+print(testPreds_cls)
+print(testPreds_cls)
+
+# Convert bounding box coordinates from normalized to pixel coordinates
+# testPreds_reg *= 224
+
+# Extract pixel coordinates for each bounding box
+testPreds_xmin = testPreds_reg[:, 0::4]
+testPreds_ymin = testPreds_reg[:, 1::4]
+testPreds_xmax = testPreds_reg[:, 2::4]
+testPreds_ymax = testPreds_reg[:, 3::4]
+
+# Convert pixel coordinates to int, this rounds it to only the int
+testPreds_xmin = testPreds_xmin.astype(int)
+testPreds_ymin = testPreds_ymin.astype(int) 
+testPreds_xmax = testPreds_xmax.astype(int)
+testPreds_ymax = testPreds_ymax.astype(int)
 
 
-# # Predict bounding boxes on the test data
-# testPreds = model.predict(testImg)
+# for i in range(len(testImg)):
+#     print('Image', i)
+#     for j in range(num_classes):
+#         if testPreds_cls[i, j] > 0.5:
+#             xmin = int(testPreds_xmin[i, j])
+#             ymin = int(testPreds_ymin[i, j])
+#             xmax = int(testPreds_xmax[i, j])
+#             ymax = int(testPreds_ymax[i, j])
+#             print('  Class', j, ':', xmin, ymin, xmax, ymax)
+
+# Print the predicted bounding boxes
+# for i in range(len(testImg)):
+#     image = testImg[i]
+#     print('Image', i)
+#     plt.imshow(image)
+#     ax = plt.gca()
+#     for j in range(num_classes):
+#         if testPreds_cls[i, j] > 0.5:
+#             xmin = int(testPreds_xmin[i])
+#             ymin = int(testPreds_ymin[i])
+#             xmax = int(testPreds_xmax[i])
+#             ymax = int(testPreds_ymax[i])
+#             print('  Class', j, ':', xmin, ymin, xmax, ymax)
+#             print('witdh: ', xmax-xmin, 'height: ', ymax-ymin)
+#             rect = plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=False, edgecolor='r')
+#             ax.add_patch(rect)
+#     plt.show()
+    
+    
+# Define a list of class names
+class_names = ["class1", "class2", "class3", "class4", "class5"]
+
+for i in range(len(testImg)):
+    image = testImg[i]
+    print('Image', i)
+    plt.imshow(image)
+    ax = plt.gca()
+    for j in range(num_classes):
+        if testPreds_cls[i, j] > 0.5:
+            # Get the coordinates of the predicted bounding box
+            xmin = testPreds_xmin[i]
+            ymin = testPreds_ymin[i]
+            xmax = testPreds_xmax[i]
+            ymax = testPreds_ymax[i]
+
+            # Draw the bounding box
+            rect = plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=False, edgecolor='r')
+            ax.add_patch(rect)
+
+            # Add the label above the bounding box
+            label = class_names[j]
+            plt.text(xmin, ymin-5, label, color='r', fontsize=12, fontweight='bold')
+
+    plt.show()
+            
+# # Print the predicted bounding boxes
+# for i in range(len(testImg)):
+#     print('Image: ', i)
+#     xmin = int(testPreds_xmin[i])
+#     ymin = int(testPreds_ymin[i])
+#     xmax = int(testPreds_xmax[i])
+#     ymax = int(testPreds_ymax[i])
+#     print('boundingBox: ', xmin, ymin, xmax, ymax)
 
 # # Extract predicted classification labels and bounding boxes
 # testPreds_cls = testPreds[:, :num_classes]
@@ -164,6 +238,7 @@ testLabels_cls = to_categorical(testLabels_cat[:, 0], num_classes)
 #     ax = plt.gca()
 #     for j in range(num_classes):
 #         if testPreds_cls[i] == j:
+#             print("here: " + testPreds_reg.shape)
 #             xmin = int(testPreds_reg[i, j*4])
 #             ymin = int(testPreds_reg[i, j*4+1])
 #             xmax = int(testPreds_reg[i, j*4+2])
@@ -171,7 +246,6 @@ testLabels_cls = to_categorical(testLabels_cat[:, 0], num_classes)
 #             rect = plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=False, edgecolor='r')
 #             ax.add_patch(rect)
 #     plt.show()
-
 
 
 
--- eindopdracht/split.py
+++ eindopdracht/split.py
@@ -5,9 +5,10 @@ import random
 
 # set up paths
 image_dir = 'data/'
-train_dir = 'dataset/train/'
-test_dir = 'dataset/test/'
-valid_dir = 'dataset/valid/'
+train_dir = 'dataset2/train/'
+test_dir = 'dataset2/test/'
+valid_dir = 'dataset2/valid/'
+new_dir = 'dataset2/new/'
 csv_path = 'dataset/_annotations.csv'
 
 # set up class whitelist
@@ -17,6 +18,7 @@ classes = {'trafficLight-Red', 'trafficLight-Green', 'trafficLight', 'car', 'tru
 os.makedirs(train_dir, exist_ok=True)
 os.makedirs(test_dir, exist_ok=True)
 os.makedirs(valid_dir, exist_ok=True)
+os.makedirs(new_dir, exist_ok=True)
 
 # read CSV file into dataframe and filter by whitelist
 df = pd.read_csv(csv_path, header=0, names=['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax'])
@@ -24,26 +26,31 @@ df = df[df['class'].isin(classes)]
 
 # randomly select unique filenames
 filenames = set(df['filename'])
-selected_filenames = set(random.sample(filenames, min(len(filenames), 6000))) # size for the randomly select
+selected_filenames = set(random.sample(filenames, min(len(filenames), 1000))) # size for the randomly select
 
 # calculate number of images for each split
-num_train = int(len(selected_filenames) * 0.6)
+num_train = int(len(selected_filenames) * 0.5)
 num_test = int(len(selected_filenames) * 0.3)
-num_valid = len(selected_filenames) - num_train - num_test
+num_valid = int(len(selected_filenames) * 0.1)
+num_new = len(selected_filenames) - num_train - num_test - num_valid
 
 # process each selected filename
 train_csv_path = os.path.join(train_dir, 'annotation.csv')
 test_csv_path = os.path.join(test_dir, 'annotation.csv')
 valid_csv_path = os.path.join(valid_dir, 'annotation.csv')
+new_csv_path = os.path.join(new_dir, 'annotation.csv')
 train_filenames = set()
 test_filenames = set()
 valid_filenames = set()
+new_filenames = set()
 with open(train_csv_path, 'w') as train_csv, \
         open(test_csv_path, 'w') as test_csv, \
-        open(valid_csv_path, 'w') as valid_csv:
+        open(valid_csv_path, 'w') as valid_csv, \
+        open(new_csv_path, 'w') as new_csv:
     train_csv.write('filename,width,height,class,xmin,ymin,xmax,ymax\n')
     test_csv.write('filename,width,height,class,xmin,ymin,xmax,ymax\n')
     valid_csv.write('filename,width,height,class,xmin,ymin,xmax,ymax\n')
+    new_csv.write('filename,width,height,class,xmin,ymin,xmax,ymax\n')
     for i, filename in enumerate(selected_filenames):
         # determine which directory to copy image to
         if i < num_train:
@@ -54,10 +61,14 @@ with open(train_csv_path, 'w') as train_csv, \
             dest_dir = test_dir
             dest_csv = test_csv
             dest_filenames = test_filenames
-        else:
+        elif i < num_train + num_test + num_valid:
             dest_dir = valid_dir
             dest_csv = valid_csv
             dest_filenames = valid_filenames
+        else:
+            dest_dir = new_dir
+            dest_csv = new_csv
+            dest_filenames = new_filenames
 
         # check if file exists and copy it to destination directory
         src_path = os.path.join(image_dir, filename)
